1ca3a5de22a3aa61ef6d351d4cca7d931f2b41dd
GamesForges <64849762+GamesForges011219@users.noreply.github.com>
2021-06-02 11:31:01 +0200

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     
-    "token": "ODM5NjA1MDM1NTUzNTIxNjg0.YJMFHw.491LpQB2nFDB6zW_SvYmthZDYgg",
-    "db_username": "gamesforges",
-    "db_password": "amelie010803",
+    "token": "Token",
+    "db_username": "db_username",
+    "db_password": "db_password",
     "db_author": "cluster0.g8kxq",
     "db_name": "tuto"
 
